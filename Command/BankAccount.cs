2cc5776f6ff92981f85fe3f4331dea06d724050d
Jake Messner <messnerdev@gmail.com>
2019-04-14 09:48:34 -0700

    Command

---
+++ Command/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Command
+{
+    public class BankAccount
+    {
+        private int _balance;
+        private int _overdraftLimit = -500;
+
+        public void Deposit(int amount)
+        {
+            _balance += amount;
+            Console.WriteLine($"Deposited ${amount}, balance is now {_balance}");
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (_balance - amount < _overdraftLimit)
+                return false;
+
+            _balance -= amount;
+            Console.WriteLine($"Withdrew ${amount}, balance is now {_balance}");
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(_balance)}: {_balance}";
+        }
+    }
+}
---
+++ Command/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ba = new BankAccount();
+            var commands = new List<ICommand>()
+            {
+                new BankAccountCommand(ba, BankAccountCommand.Action.Deposit, 100),
+                new BankAccountCommand(ba, BankAccountCommand.Action.Withdraw, 1000)
+            };
+
+            Console.WriteLine(ba);
+
+            foreach (ICommand command in commands)
+                command.Call();
+
+            Console.WriteLine(ba);
+
+            foreach (ICommand command in Enumerable.Reverse(commands))
+                command.Undo();
+
+            Console.WriteLine(ba);
+        }
+    }
+}
